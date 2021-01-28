553f8ed4dcaad714dd64a0d6b5785151ac3b226f
W4NGL33 <doubleben.fb@gmail.com>
2021-01-28 14:19:27 +0100

    Did some changes to the code

--- text.java
+++ text.java
@@ -1,5 +1,5 @@
 public class text {
-   /* static String mySchool = "University of OAGF";
+   static String mySchool = "University of OAGF";
     static String location = " in Abuja";
     static String school = mySchool + location;
 
@@ -13,7 +13,7 @@ public class text {
     static int John = jn1 * jn2;
     static int Johnm = mike1 % mike2;
     static String stringAnswer = String.valueOf(mike1) + String.valueOf(mike2);
-    static String stringAndInt = mike1 + mySchool; */
+    static String stringAndInt = mike1 + mySchool;
 
     //if statements
     static int num1 = 27;
@@ -22,26 +22,26 @@ public class text {
 
 
     public static void main(String[] args) {
-      /*  if(num1 == 27 ){
+      if(num1 == 27 ){
             num3 = "the number is 27";
         }
         else {
             num3 = "the number not 27";
-        } */
+        }
         if (num1 < 27 && num2 > 27) {
             num3 = "Its better";
         } else {
             num3 = "Its not good";
         }
-        // System.out.println(answer);
-        //System.out.println(stringAnswer);
-        // System.out.println(stringAndInt);
-        //System.out.println(mike);
-        // System.out.println(john);
-        // System.out.println(John);
-        // System.out.println(Johnm);
-        //  System.out.println(++jn2);
-        //System.out.println(--jn2);*/
+         System.out.println(answer);
+        System.out.println(stringAnswer);
+         System.out.println(stringAndInt);
+        System.out.println(mike);
+         System.out.println(john);
+         System.out.println(John);
+         System.out.println(Johnm);
+          System.out.println(++jn2);
+        System.out.println(--jn2);
         System.out.println(num3);
 
     }
