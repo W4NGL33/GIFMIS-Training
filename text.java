ebbe3f90bc3ebd526075aca89546310b76f94f4f
W4NGL33 <doubleben.fb@gmail.com>
2021-01-28 14:03:14 +0100

    This is the starting of Git

---
+++ text.java
@@ -0,0 +1,48 @@
+public class text {
+   /* static String mySchool = "University of OAGF";
+    static String location = " in Abuja";
+    static String school = mySchool + location;
+
+    static int jn1 = 300;
+    static int jn2 = 40;
+    static int mike1 = 343546;
+    static int mike2 = 353215;
+    static int answer = mike1 + mike2;
+    static int mike = mike2 - mike1;
+    static int john = jn1 / jn2;
+    static int John = jn1 * jn2;
+    static int Johnm = mike1 % mike2;
+    static String stringAnswer = String.valueOf(mike1) + String.valueOf(mike2);
+    static String stringAndInt = mike1 + mySchool; */
+
+    //if statements
+    static int num1 = 27;
+    static int num2 = 17;
+    static String num3 = "";
+
+
+    public static void main(String[] args) {
+      /*  if(num1 == 27 ){
+            num3 = "the number is 27";
+        }
+        else {
+            num3 = "the number not 27";
+        } */
+        if (num1 < 27 && num2 > 27) {
+            num3 = "Its better";
+        } else {
+            num3 = "Its not good";
+        }
+        // System.out.println(answer);
+        //System.out.println(stringAnswer);
+        // System.out.println(stringAndInt);
+        //System.out.println(mike);
+        // System.out.println(john);
+        // System.out.println(John);
+        // System.out.println(Johnm);
+        //  System.out.println(++jn2);
+        //System.out.println(--jn2);*/
+        System.out.println(num3);
+
+    }
+}
